2fc59ad40fe97faa24a3c273308d9033cb6d55e2
Matheus Dev <matheushnt.dev@gmail.com>
2025-09-05 20:14:57 -0300

    Criando a Aplicação

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import fastify from 'fastify';
+
+const app = fastify();
+
+// GET, POST, PUT, DELETE, PATCH
+
+app.get('/hello', () => {
+  return 'Hello World';
+});
+
+app.listen({ port: 3333 }).then(() => console.log('HTTP Server Running!'));
