3a8c16d3eed7d6a90f7be204e446b2ddaaa5c2a7
Matheus Dev <matheushnt.dev@gmail.com>
2025-09-06 21:12:46 -0300

    Setup definitivo

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,13 @@ import fastify from 'fastify'
 
 const app = fastify()
 
-// GET, POST, PUT, DELETE, PATCH
+app.get('/hello', (req, res) => {
+  return 'hello world'
+})
 
-app.get('/hello', () => {
-  return 'Hello World'
-});
+app.listen({
+  port: 3333,
+}).then(() => {
+  console.log('HTTP Server Running!')
+})
 
-app.listen({ port: 3333 })
-  .then(() => console.log('HTTP Server Running!'))
