68433e12e8a513308bbb50e3d0170af728180037
Matheus Dev <matheushnt.dev@gmail.com>
2025-09-10 13:16:36 -0300

    Testando a criação de transação

--- src/test/example.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from 'vitest'
-
-test('o usuário consegue criar uma nova transação', () => {
-  const responseStatusCode = 500
-
-  expect(responseStatusCode).toEqual(201)
-})
