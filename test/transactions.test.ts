785457db57723aef63e59c89fe71e0555a56fbce
Matheus Dev <matheushnt.dev@gmail.com>
2025-09-10 14:13:35 -0300

    Testando listagem de transações

--- test/transactions.test.ts
+++ test/transactions.test.ts
@@ -22,4 +22,28 @@ describe('Transactions routes', () => {
 
     expect(response.statusCode).toEqual(201)
   })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 500,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies ?? [])
+      .expect(200)
+
+    expect(listTransactionResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New Transaction',
+        amount: 500,
+      }),
+    ])
+  })
 })
